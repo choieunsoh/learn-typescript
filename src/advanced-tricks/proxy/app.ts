30db6e131c00b79a9f8a4a7544d24a52a565d8ea
Kiattipong Kamonrat <marsimadz@hotmail.com>
2024-07-14 22:07:52 +0700

    feat: add proxy example

---
+++ src/advanced-tricks/proxy/app.ts
@@ -0,0 +1,33 @@
+/* eslint-disable security/detect-object-injection */
+type User = {
+  name: string;
+  age: number;
+};
+
+const user: User = {
+  name: 'John',
+  age: 30,
+};
+
+const handler: ProxyHandler<User> = {
+  get: (target, prop: keyof User) => {
+    console.log(`Getting ${prop}`);
+    return target[prop];
+  },
+  set: (target, prop: keyof User, value: any) => {
+    if (prop === 'age' && typeof value === 'number') {
+      console.log(`Setting ${prop} to ${value}`);
+      target[prop] = value;
+    }
+    if (prop === 'name' && typeof value === 'string') {
+      console.log(`Setting ${prop} to ${value}`);
+      target[prop] = value;
+    }
+    return true;
+  },
+};
+
+const proxyUser = new Proxy(user, handler);
+console.log(proxyUser.name); // Getting name, John
+proxyUser.age = 35; // Setting age to 35
+// proxyUser.age = '35'; // Throws TypeError
