c993ded46c0147775fefe9707a151e8a66dd165c
Kiattipong Kamonrat <marsimadz@hotmail.com>
2024-07-17 11:21:10 +0700

    improve: Implement strongly-typed currying with flexible argument handling
    - Add Curry type for advanced type-safe function currying
    - Create curry function supporting partial application
    - Include example function demonstrating various currying patterns
    - Enhance type safety while allowing flexible argument combinations

--- src/strongly-typed-currying/app.ts
+++ src/strongly-typed-currying/app.ts
@@ -1,15 +1,14 @@
-type Curry<T extends (...args: any[]) => any> =
-  Parameters<T> extends [first: infer F, second: infer S, ...rest: infer R]
-    ? (input: F) => Curry<(input: S, ...rest: R) => ReturnType<T>>
-    : T;
+type Curry<F extends (...args: any[]) => any> = <T extends any[]>(
+  ...args: T
+) => T extends Parameters<F> ? ReturnType<F> : Curry<(...args: Exclude<Parameters<F>, T>[]) => ReturnType<F>>;
 
-function curry<T extends (...args: any[]) => any>(fn: T, argCount = fn.length): Curry<T> {
-  return ((...args: unknown[]) => {
-    if (args.length >= argCount) {
+function curry<F extends (...args: any[]) => any>(fn: F): Curry<F> {
+  return function curried(...args: any[]): any {
+    if (args.length >= fn.length) {
       return fn(...args);
     }
-    return curry(fn.bind(null, ...args), argCount - args.length);
-  }) as Curry<T>;
+    return (...moreArgs: any[]) => curried(...args, ...moreArgs);
+  } as Curry<F>;
 }
 
 function example(a: number, b: string, c: boolean, d: 3 | 5): { foo: string } {
@@ -18,9 +17,12 @@ function example(a: number, b: string, c: boolean, d: 3 | 5): { foo: string } {
 }
 
 const curriedExample = curry(example);
-const a = curriedExample(10);
-const b = a('foo');
-const c = b(true);
-const d = c(5);
 
-console.log(d);
+// Now you can use curriedExample with varying number of arguments
+const result1 = curriedExample(10, 'foo', true, 5);
+const result2 = curriedExample(10)('foo')(true)(5);
+const result3 = curriedExample(10, 'foo')(true, 5);
+const result4 = curriedExample(10, 'foo', true)(5);
+const result5 = curriedExample(10, 'foo')(true)(5);
+
+console.log(result1, result2, result3, result4, result5);
